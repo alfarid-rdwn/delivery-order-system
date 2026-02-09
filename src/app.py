0dd12b0d1c9f904e5fdaee2cb1470b90bee720c7
Alfarid Ridwan <alfaridridwann@gmail.com>
2026-02-09 19:28:57 +0700

    Sprint 4: add delivery order status and update action

--- src/app.py
+++ src/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
-# temporary storage (Sprint 3)
 orders = []
+order_counter = 1
 
 @app.route("/")
 def index():
@@ -11,17 +11,31 @@ def index():
 
 @app.route("/create", methods=["GET", "POST"])
 def create():
+    global order_counter
+
     if request.method == "POST":
         order = {
+            "order_id": order_counter,
             "customer_name": request.form.get("customer_name"),
-            "order_date": request.form.get("order_date")
+            "order_date": request.form.get("order_date"),
+            "status": "Pending"
         }
 
         orders.append(order)
+        order_counter += 1
 
         return redirect(url_for("index"))
 
     return render_template("create_order.html")
+
+@app.route("/update/<int:order_id>")
+def update_status(order_id):
+    for order in orders:
+        if order["order_id"] == order_id:
+            order["status"] = "Delivered"
+            break
+
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
